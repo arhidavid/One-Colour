578ad754f9a055cfc64c18efa320bd232fc3744c
arhiDavid <arhiDavid91@gmail.com>
2019-08-03 02:36:25 +0300

    Commiting files which must be discarded when branch will be merged in master

--- Source/One_Colour/One_ColourCharacter.cpp
+++ Source/One_Colour/One_ColourCharacter.cpp
@@ -171,7 +171,7 @@ void AOne_ColourCharacter::OnFire()
 	// try and play the sound if specified
 	if (FireSound != NULL)
 	{
-		UGameplayStatics::PlaySoundAtLocation(this, FireSound, GetActorLocation());
+		//UGameplayStatics::PlaySoundAtLocation(this, FireSound, GetActorLocation());
 	}
 
 	// try and play a firing animation if specified
--- Source/One_Colour/One_ColourGameMode.h
+++ Source/One_Colour/One_ColourGameMode.h
@@ -6,6 +6,14 @@
 #include "GameFramework/GameModeBase.h"
 #include "One_ColourGameMode.generated.h"
 
+UENUM()
+enum class EColour : uint8
+{
+	Red		UMETA(DisplayName = "Red"),
+	Green	UMETA(DisplayName = "Green"),
+	Blue	UMETA(DisplayName = "Blue")
+};
+
 UCLASS(minimalapi)
 class AOne_ColourGameMode : public AGameModeBase
 {
@@ -13,7 +21,7 @@ class AOne_ColourGameMode : public AGameModeBase
 
 public:
 	AOne_ColourGameMode();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = ActiveColour)
+	EColour ActiveColour = EColour::Red;
 };
-
-
-
